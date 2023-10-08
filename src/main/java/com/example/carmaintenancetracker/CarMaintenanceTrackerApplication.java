f921483c46a627b2d20c6fa18575eb13ca8dffca
Teodor Pchelinski <teodor.pchelinski@abv.bg>
2023-10-08 08:42:03 +0300

    Create project
    Creating the foundation

---
+++ src/main/java/com/example/carmaintenancetracker/CarMaintenanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.carmaintenancetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarMaintenanceTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarMaintenanceTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/carmaintenancetracker/CarMaintenanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carmaintenancetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarMaintenanceTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
