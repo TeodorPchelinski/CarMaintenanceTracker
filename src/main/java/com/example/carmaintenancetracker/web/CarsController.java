b1a0c134559dd5403f93af1f8e4a4329b31c1524
Teodor Pchelinski <teodor.pchelinski@abv.bg>
2023-12-24 09:25:40 +0200

    Get User Credentials still working on it

--- src/main/java/com/example/carmaintenancetracker/web/CarsController.java
+++ src/main/java/com/example/carmaintenancetracker/web/CarsController.java
@@ -3,13 +3,14 @@ package com.example.carmaintenancetracker.web;
 import com.example.carmaintenancetracker.model.dto.CreateCarDTO;
 import com.example.carmaintenancetracker.service.CarService;
 import jakarta.validation.Valid;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.yaml.snakeyaml.events.Event;
 
 @Controller
 public class CarsController {
@@ -29,6 +30,18 @@ public class CarsController {
     public String add(@Valid CreateCarDTO createCarDTO,
                       BindingResult bindingResult,
                       RedirectAttributes redirectAttributes) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication == null || !authentication.isAuthenticated()) {
+
+            return "redirect:/login";
+        }
+
+        //todo: usedId when creating a car
+
+        String userId = authentication.getName();
+
+
 
         if (bindingResult.hasErrors()) {
             redirectAttributes.addFlashAttribute("createCarDTO", createCarDTO);
